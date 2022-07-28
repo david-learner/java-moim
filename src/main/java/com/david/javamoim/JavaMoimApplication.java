11caef7b84395df0806a42d25a92832021b52f08
david <xodnjs2549@naver.com>
2022-07-28 19:35:26 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/david/javamoim/JavaMoimApplication.java
@@ -0,0 +1,13 @@
+package com.david.javamoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMoimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/javamoim/JavaMoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.javamoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMoimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
